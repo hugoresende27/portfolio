1f53880d54e7aca60da2cd18295957e4951b0b6b
hugoresende27 <Hugoresende27@gmail.com>
2021-12-05 17:23:06 +0000

    a

---
+++ scripts/animatedTextProjetos_php.js
@@ -0,0 +1,17 @@
+const textEl2 = document.getElementById('textoProjetos')
+const tituloProjetos = "Alguns projetos meus em PHP"
+let index2 = 1
+let speed2 = 200 
+
+escreveTexto2()
+
+function escreveTexto2() {
+    textEl2.innerText = tituloProjetos.slice(0, index2)
+
+    index2++
+    if ( index2 > tituloProjetos.length) {/*PARA COMEÇAR A ESCREVER DE NOVO QUANDO CHEGAR AO FIM DO TEXTO */
+        index2 = 1
+    }
+
+    setTimeout(escreveTexto2 , speed2)
+}
